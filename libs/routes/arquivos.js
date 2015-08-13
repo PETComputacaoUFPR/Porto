267a8343c35413b7f41406610ff6ec084b7d8c65
VytorCalixto <vytorcalixto@gmail.com>
2015-08-12 21:58:14 -0300

    Autenticação em algumas outras rotas de arquivos

--- libs/routes/arquivos.js
+++ libs/routes/arquivos.js
@@ -3,13 +3,14 @@ var passport = require('passport')
 var router = express.Router()
 var multer = require('multer')
 var upload = multer({dest: 'uploads/'})
+var fs = require('fs')
 
 var libs = process.cwd() + '/libs/'
 
 var db = require(libs + 'db/mongoose')
 var Arquivo = require(libs + 'model/arquivo')
 
-router.get('/', function(req, res) {
+router.get('/', passport.authenticate('bearer', { session: false }), function(req, res) {
     Arquivo.find()
         .populate('materia')
         .populate('professor')
@@ -25,7 +26,7 @@ router.get('/', function(req, res) {
         })
 })
 
-router.get('/:id', function(req, res) {
+router.get('/:id', passport.authenticate('bearer', { session: false }), function(req, res) {
     Arquivo.findById(req.params.id)
         .populate('materia')
         .populate('professor')
@@ -45,7 +46,7 @@ router.get('/:id', function(req, res) {
         })
 })
 
-router.get('/status/:status', function(req, res) {
+router.get('/status/:status', passport.authenticate('bearer', { session: false }), function(req, res) {
     Arquivo.find({status: req.params.status})
         .populate('materia')
         .populate('professor')
@@ -62,6 +63,10 @@ router.get('/status/:status', function(req, res) {
 })
 
 router.post('/', upload.array('files', 1), passport.authenticate('bearer', { session: false }), function(req, res) {
+    /*
+    No momento é feito o upload de apenas 1 arquivo. Logo esse for é "inútil"
+    Mas, na verdade, este for está já está pensando quando vierem multiplas imagens
+    */
     for(var f in req.files) {
         file = req.files[f]
         var arquivo = new Arquivo({
@@ -88,15 +93,16 @@ router.post('/', upload.array('files', 1), passport.authenticate('bearer', { ses
                     res.statusCode = 500
                     res.json({error:'Server error'})
                 }
-                console.log(err)
                 console.log('Internal error(%d): %s', res.statusCode, err.message)
+                fs.unlink(file.path, function() {
+                    if(err) console.log(err)
+                })
             }
         })
     }
 })
 
 router.put('/:id', passport.authenticate('bearer', { session: false }), function(req, res) {
-    var memberId = req.params.id
 
     Arquivo.findById(memberId, function(err, arquivo) {
         if(!arquivo) {
