068934f12ae7565002b9ce09a7ac9fb9d1ca83e5
VytorCalixto <vytorcalixto@gmail.com>
2015-08-14 09:51:34 -0300

    Adicionado process.env.PORT como opção para porta

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var config = require(libs + 'config')
 var log = require(libs + 'log')(module)
 var app = require(libs + 'app')
 
-app.set('port', config.get('port') || 3000)
+app.set('port', process.env.PORT || config.get('port') || 3000)
 
 var server = app.listen(app.get('port'), function() {
   debug('Express server listening on port ' + server.address().port)
