7082c61f00db56e0985b35f08e863a14df7f0e36
VytorCalixto <vytorcalixto@gmail.com>
2015-08-14 10:01:01 -0300

    Removido arquivo do log

--- libs/log.js
+++ libs/log.js
@@ -6,15 +6,15 @@ function logger(module) {
 
     return new winston.Logger({
         transports : [
-            new winston.transports.File({
-                level: 'info',
-                filename: process.cwd() + '/logs/all.log',
-                handleException: true,
-                json: true,
-                maxSize: 5242880, //5mb
-                maxFiles: 2,
-                colorize: false
-            }),
+            // new winston.transports.File({
+            //     level: 'info',
+            //     filename: process.cwd() + '/logs/all.log',
+            //     handleException: true,
+            //     json: true,
+            //     maxSize: 5242880, //5mb
+            //     maxFiles: 2,
+            //     colorize: false
+            // }),
             new winston.transports.Console({
                 level: 'debug',
                 label: getFilePath(module),
